dbf1967271339796d0667036cba2c59d9f71aec7
haythembahry <haythembahri16@gmail.com>
2023-06-08 16:10:53 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,82 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    import nltk
+
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('stopwords')
+    nltk.download('wordnet')
+    from nltk.tokenize import word_tokenize, sent_tokenize
+    from nltk.corpus import stopwords
+    from nltk.stem import WordNetLemmatizer
+    import string
+    import streamlit as st
+
+    # Load the text file and preprocess the data
+    with open('C:/Users/x/OneDrive/Bureau/test.txt', 'r', encoding='utf-8') as f:
+        data = f.read().replace('\n', ' ')
+    # Tokenize the text into sentences
+    sentences = sent_tokenize(data)
+
+
+    # Define a function to preprocess each sentence
+    def preprocess(sentence):
+        # Tokenize the sentence into words
+        words = word_tokenize(sentence)
+        # Remove stopwords and punctuation
+        words = [word.lower() for word in words if
+                 word.lower() not in stopwords.words('english') and word not in string.punctuation]
+        # Lemmatize the words
+        lemmatizer = WordNetLemmatizer()
+        words = [lemmatizer.lemmatize(word) for word in words]
+        return words
+
+
+    # Preprocess each sentence in the text
+    corpus = [preprocess(sentence) for sentence in sentences]
+
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Define a function to find the most relevant sentence given a query
+def get_most_relevant_sentence(query):
+    # Preprocess the query
+    query = preprocess(query)
+    # Compute the similarity between the query and each sentence in the text
+    max_similarity = 0
+    most_relevant_sentence = ""
+    for sentence in corpus:
+        similarity = len(set(query).intersection(sentence)) / float(len(set(query).union(sentence)))
+        if similarity > max_similarity:
+            max_similarity = similarity
+            most_relevant_sentence = " ".join(sentence)
+    return most_relevant_sentence
+def chatbot(question):
+    # Find the most relevant sentence
+    most_relevant_sentence = get_most_relevant_sentence(question)
+    # Return the answer
+    return most_relevant_sentence
+# Create a Streamlit app
+def main():
+    st.title("Chatbot")
+    st.write("Hello! I'm a chatbot. Ask me anything about the topic in the text file.")
+    # Get the user's question
+    question = st.text_input("You:")
+    # Create a button to submit the question
+    if st.button("Submit"):
+        # Call the chatbot function with the question and display the response
+        response = chatbot(question)
+        st.write("Chatbot: " + response)
+if __name__ == "__main__":
+    main()
